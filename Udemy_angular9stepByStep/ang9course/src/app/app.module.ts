e6b8e0dd7bd8d0ff5dccf04677966bc4888a3834
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2021-01-21 22:32:57 +0700

    ep7 routerLink
    ep7 routerLink

--- Udemy_angular9stepByStep/ang9course/src/app/app.module.ts
+++ Udemy_angular9stepByStep/ang9course/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { NavComponent } from './nav/nav.component';
+import { RouterModule } from '@angular/router'; // import เข้ามา เพื่อ ทำ router link
 
 @NgModule({
   declarations: [
@@ -15,12 +16,18 @@ import { NavComponent } from './nav/nav.component';
     ProductComponent,
     ContactComponent,
     AboutComponent,
-    NavComponent
+    NavComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'product', component: ProductComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
