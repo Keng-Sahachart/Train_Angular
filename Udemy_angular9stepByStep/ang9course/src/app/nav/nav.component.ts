afa9d48d939fa93b5b2beccadef9be84c7d0c126
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2021-01-22 13:24:27 +0700

    ep 9 10 oneway binging

--- Udemy_angular9stepByStep/ang9course/src/app/nav/nav.component.ts
+++ Udemy_angular9stepByStep/ang9course/src/app/nav/nav.component.ts
@@ -3,18 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  labelHome = 'Home';
+  labelProduct = 'Product';
+  labelContact = 'Contact';
+  labelAbout = 'About';
 
-  labelHome = "Home";
-  labelProduct = "Product";
-  labelContact = "Contact";
-  labelAbout = "About";
+  /* test one way binging */
+  labels = {
+    home: 'Home',
+    product: 'Product',
+    contact: 'Contact',
+    about: 'About',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
